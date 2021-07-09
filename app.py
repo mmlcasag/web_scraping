2db733deaf0c6f97515a8601ed3772683d437e03
Marcio Casagrande <marcio.casagrande@roundpe.gs>
2021-07-09 19:24:02 -0300

    Loading in our web page

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import requests
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 
+# Load the webpage content
+r = requests.get("https://keithgalli.github.io/web-scraping/example.html")
+
+# Convert to a BeautifulSoup object
+soup = BeautifulSoup(r.content, "html.parser")
+# the second argument is to avoid the BeautifulSoup warning message
+# https://stackoverflow.com/questions/33511544/how-to-get-rid-of-beautifulsoup-user-warning
+
+# Print out our HTML
+print(soup)
+
+# Print out our HTML formatted in a more readable way
+print(soup.prettify())
