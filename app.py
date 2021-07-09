2fe828e19510d50c0a7126f68f914c3e3ea70fd2
Marcio Casagrande <marcio.casagrande@roundpe.gs>
2021-07-09 19:48:01 -0300

    Learning find() and find_all() functions

--- app.py
+++ app.py
@@ -14,3 +14,61 @@ print(soup)
 
 # Print out our HTML formatted in a more readable way
 print(soup.prettify())
+
+# Start using BeautifulSoup to scrape
+first_header = soup.find("h2")
+print('find("h2") example:')
+print(first_header)
+print("")
+# Notice that we have 2 <h2> tags in the page
+# The find function returns only the first occurrence
+
+# If you want all the occurrences you should use the find_all function
+print('find_all("h2") example:')
+headers = soup.find_all("h2")
+print(headers)
+print("")
+
+# Now you can loop through all of them
+print('Looping through the headers:')
+for header in headers:
+    print(header)
+print("")
+
+# You can also pass in a list of elements to look for
+print('find(["h1","h2"]) example:')
+first_header = soup.find(["h1","h2"])
+print(first_header)
+print("")
+
+print('find_all(["h1","h2"]) example:')
+all_headers = soup.find_all(["h1","h2"])
+print(all_headers)
+print("")
+
+# Let's say you want to find all paragraphs within the web page
+print('find_all("p") example:')
+paragraphs = soup.find_all("p")
+print(paragraphs)
+print("")
+
+# You can pass in attributes to the find/find_all function
+# Let's say you now want only the paragraph with the id from the previous example
+paragraph = soup.find_all("p", attrs={"id": "paragraph-id"})
+print(paragraph)
+print("")
+
+# You can nest find/find_all calls
+body = soup.find("body")
+print(body)
+print("")
+# Here you have the entire body element
+# But now let's say you want only the first div tag inside the body
+# You can do this
+div = body.find("div")
+print(div)
+print("")
+# Now let's extract the header from the div
+header = div.find("h1")
+print(header)
+print("")
